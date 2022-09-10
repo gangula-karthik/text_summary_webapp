a1b1da04a1c215f4066ae34dac8c1f4b9174bfda
gangula-karthik <gkarthik923@gmail.com>
2022-09-10 13:02:51 +0800

    add files

---
+++ text_summary_app.py
@@ -0,0 +1,13 @@
+import streamlit
+
+streamlit.title("Text Summary App 🚀")
+
+streamlit.header("Enter your text below:")
+text = streamlit.text_area("")
+streamlit.header("Summary:")
+streamlit.write(text)
+
+
+# Run the app
+if __name__ == "__main__":
+    streamlit.run()
