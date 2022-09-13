458fdc3ef16e340da55b2239bd8b36b322fe1336
gangula-karthik <gkarthik923@gmail.com>
2022-09-13 20:56:48 +0800

    fix: empty msg entered

--- text_summary_app.py
+++ text_summary_app.py
@@ -6,9 +6,10 @@ streamlit.title("Text Summary App 📚")
 streamlit.header("Enter your text below:")
 text = streamlit.text_area("")
 streamlit.header("Summary:")
-summarizer = pipeline("summarization")
-hf_summary = summarizer(text, max_length= 500, min_length= 100, do_sample= False, truncation=True)
-streamlit.write(hf_summary[0]['summary_text'])
+if text != "":
+    summarizer = pipeline("summarization")
+    hf_summary = summarizer(text, max_length= 500, min_length= 100, do_sample= False, truncation=True)
+    streamlit.write(hf_summary[0]['summary_text'])
 
 
 
